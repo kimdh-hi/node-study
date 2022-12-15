4926a9390eabf32927d411f239eadb37f1c21375
daehyunKim <zbeld123@gmail.com>
2022-12-15 20:57:38 +0900

    feat(node): request routing, parse form input

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,7 +1,41 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+
+    const url = req.url;
+    const method = req.method;
+
+    console.log(url, method)
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<form action="/form" method="POST"><input type="text" name="test"><button type="submit">submit</button></input></form>')
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/form' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log('chunk:', chunk);
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log('parsedBody: ', parsedBody);
+
+            const inputValue = parsedBody.split('=')[1];
+            fs.writeFileSync('testFile.txt', inputValue);
+        })
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
 });
 
 server.listen(3000);
